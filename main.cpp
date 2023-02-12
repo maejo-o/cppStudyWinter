48b38813055dbff3a28f0e2c5f04135f099b5507
maejo-o <efforterjisulee@gmail.com>
2023-02-13 04:53:12 +0900

    second EmployeeManager

--- main.cpp
+++ main.cpp
@@ -2,23 +2,36 @@
 # include <cstring>
 using namespace std;
 
-class PermanentWorker
+class Employee
 {
 private:
     char name[100];
-    int salary;
 public:
-    PermanentWorker(char* name, int money)
-    : salary(money)
+    Employee(char *name)
     {
         strcpy(this->name, name);
     }
-    int GetPay() const{
+    void ShowYourName() const
+    {
+        cout<<"name : "<<name<<endl;
+    }
+};
+
+class PermanentWorker : public Employee
+{
+private:
+    int salary;
+public:
+    PermanentWorker(char* name, int money)
+    : Employee(name), salary(money)
+    { }
+    int GetPay() const
+    {
         return salary;
     }
     void ShowSalaryInfo() const
     {
-        cout<<"name : "<<name<<endl;
+        ShowYourName();
         cout<<"salary : "<<GetPay()<<endl<<endl;
     }
 };
@@ -26,28 +39,32 @@ public:
 class EmployeeHandler
 {
 private:
-    PermanentWorker* empList[50];
+    Employee* empList[50];
     int empNum;
 public:
     EmployeeHandler() :empNum(0)
     { }
-    
     void AddEmployee(PermanentWorker* emp)
     {
         empList[empNum++]=emp;
     }
-    
     void ShowAllSalaryInfo() const
     {
+        /*
         for (int i=0; i<empNum; i++)
             empList[i]->ShowSalaryInfo();
+         */
     }
     
     void ShowTotalSalary() const
     {
         int sum=0;
+        /*
+        int sum=0;
         for (int i=0; i<empNum; i++)
             sum+=empList[i]->GetPay();
+        cout<<"salary sum : "<<sum<<endl;
+         */
         cout<<"salary sum : "<<sum<<endl;
     }
     
