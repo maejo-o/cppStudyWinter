1c1c5a8ceda1e4ef2919546ce9afb2fc71e9829e
maejo-o <efforterjisulee@gmail.com>
2023-02-12 14:56:59 +0900

    DerivDestOrder.cpp

--- main.cpp
+++ main.cpp
@@ -6,17 +6,12 @@ class SoBase
 private:
     int baseNum;
 public:
-    SoBase() : baseNum(20)
-    {
-        cout<<"SoBase()"<<endl;
-    }
     SoBase(int n) : baseNum(n)
     {
-        cout<<"SoBase(int n)"<<endl;
-    }
-    void ShowBaseData()
+        cout<<"SoBase() : "<<baseNum<<endl;
+    }~SoBase()
     {
-        cout<<baseNum<<endl;
+        cout<<"~SoBase() : "<<baseNum<<endl;
     }
 };
 
@@ -25,37 +20,19 @@ class SoDerived : public SoBase
 private:
     int derivNum;
 public:
-    SoDerived() : derivNum(30)
+    SoDerived(int n) : SoBase(n), derivNum(n)
     {
-        cout<<"SoDerived()"<<endl;
+        cout<<"SoDerived() : "<<derivNum<<endl;
     }
-    SoDerived(int n) : derivNum(n)
+    ~SoDerived()
     {
-        cout<<"SoDerived(int n)"<<endl;
-    }
-    SoDerived(int n1, int n2) : SoBase(n1), derivNum(n2)
-    {
-        cout<<"SoDerived(int n1, int n2)"<<endl;
-    }
-    void ShowDerivData()
-    {
-        ShowBaseData();
-        cout<<derivNum<<endl;
+        cout<<"~SoDerived() : "<<derivNum<<endl;
     }
 };
 
 int main(void)
 {
-    cout<<"case 1........."<<endl;
-    SoDerived dr1;
-    dr1.ShowDerivData();
-    cout<<"-----------------"<<endl;
-    cout<<"case 2........."<<endl;
-    SoDerived dr2(12);
-    dr2.ShowDerivData();
-    cout<<"-----------------"<<endl;
-    cout<<"case 3 ........"<<endl;
-    SoDerived dr3(23, 24);
-    dr3.ShowDerivData();
+    SoDerived dr1(15);
+    SoDerived dr2(27);
     return 0;
 }
